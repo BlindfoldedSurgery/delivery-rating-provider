4aa628b4de64ca73f9f9da051419298c08dd5d0c
Torben Carstens <git@carstens.tech>
2024-01-13 13:21:29 +0100

    Update tests to new get_random_restaurants method

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -6,18 +6,12 @@ from provider.bot import get_restaurant_list_url, get_random_restaurants
 @pytest.mark.asyncio
 async def test_restaurant_retrieval_is_working_without_filter():
     url = get_restaurant_list_url(postal_code="64293")
-    try:
-        await get_random_restaurants(url, count=1)
-        assert True
-    except IndexError:
-        assert False
+    restaurants = await get_random_restaurants(url, count=1)
+    assert len(restaurants) > 0
 
 
 @pytest.mark.asyncio
 async def test_restaurant_retrieval_is_working_with_filter():
     url = get_restaurant_list_url(postal_code="64293")
-    try:
-        await get_random_restaurants(url, count=1, filter_fn=lambda _: True)
-        assert True
-    except IndexError:
-        assert False
+    restaurants = await get_random_restaurants(url, count=1, filter_fn=lambda _: True)
+    assert len(restaurants) > 0
