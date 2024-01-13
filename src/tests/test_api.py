6bbabe94678f1d551cef7fe31f2700feb4c44f23
Torben Carstens <git@carstens.tech>
2024-01-13 14:22:59 +0100

    Fix type in tests

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -5,13 +5,13 @@ from provider.bot import get_restaurant_list_url, get_random_restaurants
 
 @pytest.mark.asyncio
 async def test_restaurant_retrieval_is_working_without_filter():
-    url = get_restaurant_list_url(postal_code="64293")
+    url = get_restaurant_list_url(postal_code=64293)
     restaurants = await get_random_restaurants(url, count=1)
     assert len(restaurants) > 0
 
 
 @pytest.mark.asyncio
 async def test_restaurant_retrieval_is_working_with_filter():
-    url = get_restaurant_list_url(postal_code="64293")
+    url = get_restaurant_list_url(postal_code=64293)
     restaurants = await get_random_restaurants(url, count=1, filter_fn=lambda _: True)
     assert len(restaurants) > 0
